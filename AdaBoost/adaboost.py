024cfc872ffc1ac33a2bc6ffb523d153e6537202
caylaxu <306538517@qq.com>
2018-02-06 21:48:54 +0800

    adaboost

--- AdaBoost/adaboost.py
+++ AdaBoost/adaboost.py
@@ -3,15 +3,6 @@
 
 """
 基于单层决策树构建弱分类器
-
-将最小错误率 minError 设为+∞
-对数据集中的每一个特征(第一层循环):
-    对每个步长(第二层循环):
-        对每个不等号(第三层循环):
-            建立一棵单层决策树并利用加权数据集对它进行测试
-            如果错误率低于 minError ,则将当前单层决策树设为最佳单层决策树
-返回最佳单层决策树
-
 @author: caylaxu
 @contact: xmy306538517@foxmail.com
 @time: 18-1-5 下午9:36
@@ -29,7 +20,18 @@ def loadSimpData():
     return datMat, classLabels
 
 
-###############单层决策树生成函数################
+###############基于单层决策树构建弱分类器################
+"""
+将最小错误率 minError 设为+∞
+对数据集中的每一个特征(第一层循环):
+    对每个步长(第二层循环):
+        对每个不等号(第三层循环):
+            建立一棵单层决策树并利用加权数据集对它进行测试
+            如果错误率低于 minError ,则将当前单层决策树设为最佳单层决策树
+返回最佳单层决策树
+"""
+
+
 def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
     retArray = ones((shape(dataMatrix)[0], 1))
     if threshIneq == 'lt':
@@ -71,7 +73,74 @@ def buildStump(dataArr, classLabels, D):
     return bestStump, minError, bestClassEst
 
 
+##########完整 AdaBoost 算法的实现##########
+"""
+对每次迭代:
+    利用 buildStump() 函数找到最佳的单层决策树
+    将最佳单层决策树加入到单层决策树数组
+    计算alpha
+    计算新的权重向量 D
+    更新累计类别估计值
+    如果错误率等于0.0,则退出循环
+"""
+
+
+def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m, 1)) / m)
+    aggClassEst = mat(zeros((m, 1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print "D:", D.T
+        alpha = float(0.5 * log((1.0 - error) / max(error, 1e-16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print "classEst:", classEst.T
+        # 1. 为下一次迭代计算D
+        expon = multiply(-1 * alpha * mat(classLabels).T, classEst)
+        D = multiply(D, exp(expon))
+        D = D / D.sum()
+        # 2. 错误率累计
+        aggClassEst += alpha * classEst
+        print "aggClassEst: ", aggClassEst.T
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T, ones((m, 1)))
+        errorRate = aggErrors.sum() / m
+        print "total error: ", errorRate, "\n"
+        if errorRate == 0.0:
+            break
+    return weakClassArr
+
+def adaClassify(datToClass,classifierArr):
+    """
+    AdaBoost分类函数
+    :param datToClass:
+    :param classifierArr:
+    :return:
+    """
+    dataMatrix = mat(datToClass)
+    m = shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix,classifierArr[i]['dim'],\
+                                 classifierArr[i]['thresh'],\
+                                 classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha']*classEst
+        print aggClassEst
+    return sign(aggClassEst)
+
+
 if __name__ == '__main__':
     datMat, classLabels = loadSimpData()
-    D = mat(ones((5, 1)) / 5)
-    buildStump(datMat, classLabels, D)
+    # test1 基于单层决策树的弱分类器
+    # D = mat(ones((5, 1)) / 5)
+    # buildStump(datMat, classLabels, D)
+
+    # test2 基于单层决策树的AdaBoost分类器
+    classifierArr = adaBoostTrainDS(datMat,classLabels,30)
+    adaClassify([0,0],classifierArr)
+
+
+
+
+
